b7cf290a780d0d59938be1323b7402748e5510f9
umutunsal <42064408+umutunsal@users.noreply.github.com>
2024-10-09 02:34:54 +0300

    Update uniswap agent with pinecone index

--- uniswap-retrieval-agent/uniswap-retrieval-agent.py
+++ uniswap-retrieval-agent/uniswap-retrieval-agent.py
@@ -1,11 +1,46 @@
 from hive_agent import HiveAgent
 import os
 import logging
+import subprocess
+import shutil
 
 logging.basicConfig(level=logging.INFO)
 
 def get_config_path(filename):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), filename))
+
+document_path = 'hive-agent-data/files/user'
+project = 'uniswap-docs'
+repo_url = 'https://github.com/Uniswap/docs.git'
+
+print(f"Cloning docs for {project}: {repo_url}")
+
+project_repo_dir = os.path.join(document_path, project)
+if not os.path.exists(project_repo_dir):
+    subprocess.run(f"mkdir {project_repo_dir}", shell=True)
+    subprocess.run(
+        f'git clone {repo_url} {project_repo_dir}',
+        shell=True
+    )
+
+print(f"Cloned {project}({repo_url}) to {project_repo_dir}")
+
+documentation_files = ['docs']
+
+remove_list = [item for item in os.listdir(project_repo_dir) if item not in documentation_files]
+
+for item in remove_list:
+    item_path = os.path.join(project_repo_dir, item)
+    
+    if os.path.isfile(item_path):
+        os.remove(item_path)
+        print(f'Removed file: {item_path}')
+    elif os.path.isdir(item_path):
+        shutil.rmtree(item_path)
+        print(f'Removed directory: {item_path}')
+    else:
+        print(f'Item not found: {item_path}')
+
 
 instruction = """ Welcome to the Uniswap Documentation Q&A Assistant! Your role is to provide accurate and concise answers to user queries based on the official Uniswap documentation. When responding, keep the following guidelines in mind:
 
@@ -22,5 +57,7 @@ my_agent = HiveAgent(
     config_path=get_config_path("hive_config.toml"),
     retrieve=True,
     required_exts=[".md"],
+    retrieval_tool = 'pinecone-serverless',
+    index_name="uniswap-protocol",
 )
 my_agent.run()
